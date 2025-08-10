c37f36903e77ab3e5c7666bab5f1868fbc84ecc4
EC2 Default User <ec2-user@ip-10-0-31-124.ec2.internal>
2025-08-10 18:48:25 +0000

    increased lambda timeout and ec2 inactivity threshold

--- main.tf
+++ main.tf
@@ -100,6 +100,7 @@ resource "aws_lambda_function" "stop_ec2" {
   role          = aws_iam_role.lambda_exec.arn
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.11"
+  timeout       = 30
   source_code_hash = filebase64sha256(data.archive_file.lambda_zip.output_path)
   environment {
     variables = {
